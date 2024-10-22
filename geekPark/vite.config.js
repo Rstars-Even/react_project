b75947790aa4780046d4dfd3639afa9ab80c0c74
Rstars-Even <473753137@qq.com>
2024-10-22 16:34:06 +0800

    项目完成。

--- geekPark/vite.config.js
+++ geekPark/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
-  plugins: [react({})],
+  base: './',
+  plugins: [react({}), visualizer(
+    {
+      open: true,// 打包后自动打开页面
+      gzipSize: true,// 查看 gzip 压缩大小
+      brotliSize: true// 查看 brotli 压缩大小
+    }
+  )],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
