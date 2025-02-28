5f61637c5d96f227987679a0064774c0d5c049e7
Rstars-Even <473753137@qq.com>
2025-03-01 00:13:42 +0800

    react-ts项目添加 tailwindcss

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from 'antd';
 
 function App() {
 
   return (
-    <>
-      <Button type="primary">Button</Button>
-    </>
+    <div className="flex justify-center items-center h-screen bg-blue-200">
+      <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
+    </div>
   )
 }
 
