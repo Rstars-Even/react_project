abbc64a592a221ea5f98bfa57fa29827ab7a5478
Rstars-Even <473753137@qq.com>
2025-03-08 23:03:52 +0800

    nasdaq 项目注册接口完成。

--- react-project/Server/nasdaq/routes/web.php
+++ react-project/Server/nasdaq/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\VideoController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,5 @@ Route::get('/', function () {
 Route::apiResource('lesson', LessonController::class);
 Route::apiResource('chapter', ChapterController::class);
 Route::apiResource('video', VideoController::class);
+// 登录注册接口
+Route::post('auth/register', [AuthController::class, 'register']);
